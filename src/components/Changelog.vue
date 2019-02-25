cb78f4496a18e130faf2ba93303ee85512225149
David Mödinger <david.moedinger@ketzu.net>
2019-02-25 20:33:38 +0100

    Changelog entry 0.8.4

--- src/components/Changelog.vue
+++ src/components/Changelog.vue
@@ -46,15 +46,18 @@
           <v-flex md12>
             <h2>Changelog</h2>
 
-            <div slot="header">Version 0.8.3</div>
+            <div slot="header">Version 0.8.4</div>
             <ul>
-              <li>City name can now be changed.</li>
-              <li>Preview for City Grid included, major feature of 0.9.</li>
+              <li>Multi buy until next update+rework</li>
+              <li>Starting Picture Font change</li>
+              <li>Welcome screen for new players</li>
+              <li>Still: Preview for City Grid included, major feature of 0.9.</li>
             </ul>
             <v-expansion-panel>
               <v-expansion-panel-content>
                 <div slot="header">Version 0.8</div>
                 <ul>
+                  <li>City name can now be changed.</li>
                   <li>Feedback directly on the side.</li>
                   <li>Changelog expandable and reduction to main versions.</li>
                   <li>Notifications now stack instead of overwrite.</li>
