f4332287a194b6c83c9f2c18eaba7277c22490e3
David Mödinger <david.moedinger@ketzu.net>
2019-02-26 19:54:57 +0100

    feedback is working now...

--- public/readfeedback.php
+++ public/readfeedback.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <link rel="icon" href="<%= BASE_URL %>favicon.ico">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <title>City Idle</title>
-</head>
-<body style="background-color:#2e7d32;">
-
-    <div class="container">
-
-    </div> <!-- /container -->
 <?php
 ########## Database Info ##########
 $db_server = 'rdbms.strato.de';
@@ -24,7 +9,21 @@ $cookiename = 'DSA374960Blub';
 ########## READ DATA
 
 $password = $_POST["password"]?: '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>City Idle</title>
+</head>
+<body style="background-color:#2e7d32;color:white;font-size: large;">
 
+    <div class="container">
+    <?php
 if($password=="FUCKOFF666") {
     // Create connection
     $mysqli = new mysqli($db_server, $db_user, $db_passwd, $db_name);
@@ -32,19 +31,20 @@ if($password=="FUCKOFF666") {
         die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
     }
 
-    $sql = "SELECT * FROM MyGuests";
+    $sql = "SELECT * FROM SI_feedback";
     $result = $mysqli->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "<div class='row'>".$row["message"]."</div>";
+            echo "<div class='row' style='margin:15px;'><div class='.col-md-6 .offset-md-3'>".$row["message"]."</div></div>";
         }
     } else {
         echo "<div class='row'>0 results</div>";
     }
     $mysqli->close();
 }
-?>
+    ?>
+    </div>
 </body>
 </html>
