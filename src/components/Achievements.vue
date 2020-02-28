8759e35ce5ef2566ed04ccc9e7d1e068becf7947
David Mödinger <david.moedinger@ketzu.net>
2020-02-28 18:22:32 +0100

    Achievement layout fixed.

--- src/components/Achievements.vue
+++ src/components/Achievements.vue
@@ -1,19 +1,15 @@
 <template>
-  <v-container grid-list-lg>
-    <v-layout align-start justify-center row wrap>
-      <v-flex md4 offset-md1 xs10 offset-xs1 :key="index" v-for="(achievement, index) in achievements">
-        <v-layout align-center justify-left row wrap>
-          <v-flex md3>
-            <v-icon large :color="cleared(index)?achievement.iconcolor:'grey lighten-2'" style="width:40px;">{{achievement.icon}}</v-icon>
-          </v-flex>
-          <v-flex md9>
-            <h2 class="stat">{{achievement.title}}</h2>
-            <span class="subitem">{{achievement.desc}} </span><br v-if="cleared(index)">
-            <span class="subitem" v-if="cleared(index)">{{(achievement.buildingboni!==undefined?achievement.buildingboni:'Improvement: '+achievement.mult*100+'%.')}}</span>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+  <v-container>
+    <v-row wrap :key="index" v-for="(achievement, index) in achievements">
+      <v-col md="3" class="text-center">
+        <v-icon large :color="cleared(index)?achievement.iconcolor:'grey lighten-2'" style="width:40px;">{{achievement.icon}}</v-icon>
+      </v-col>
+      <v-col md="9">
+        <h2 class="stat">{{achievement.title}}</h2>
+        <span class="subitem">{{achievement.desc}} </span><br v-if="cleared(index)">
+        <span class="subitem" v-if="cleared(index)">{{(achievement.buildingboni!==undefined?achievement.buildingboni:'Improvement: '+achievement.mult*100+'%.')}}</span>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
